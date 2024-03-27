9bbea238befd39cfaac938833362237bcb278872
KOTLIN <acarolinavilel@gmail.com>
2024-03-27 15:22:39 -0300

    quarta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,20 @@ import { AppRoutingModule } from './app.routing';
 import { IndexModule } from './parts/index/index.module';
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     CommonModule,
     BrowserModule,
     AppRoutingModule,
+    IndexModule,
+    BrowserAnimationsModule,
+    MatMenuModule
 
   ],
   providers: [
--- src/app/parts/index/index.module.ts
+++ src/app/parts/index/index.module.ts
@@ -2,16 +2,32 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IndexComponent } from './index.component';
 import { IndexRoutingModule } from './index.routing';
+import { register } from 'swiper/element/bundle';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { MatMenuModule } from '@angular/material/menu';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
 
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import{MatListModule} from '@angular/material/list';
 
+register()
 
 @NgModule({
   declarations: [
     IndexComponent
   ],
   imports: [
-    CommonModule,
-    IndexRoutingModule
+CommonModule,
+    IndexRoutingModule,
+    MatMenuModule,
+    MatBadgeModule,
+    MatSidenavModule,
+    MatListModule
+  ],
+  schemas:[
+    CUSTOM_ELEMENTS_SCHEMA
   ]
 })
 export class IndexModule { }
